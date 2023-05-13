d5becfcfdf48b58e89d81bc78d0774f0d519a180
JonnerPaz <jonnerpazp@gmail.com>
2023-05-13 15:47:59 -0400

    Set up color

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -11,7 +11,7 @@ function ColorMyPencils(color)
 			light = "latte",
 			dark = "mocha",
 		},
-		transparent_background = true,
+		transparent_background = false,
 		show_end_of_buffer = true, -- show the '~' characters after the end of buffers
 		term_colors = false,
 		dim_inactive = {
