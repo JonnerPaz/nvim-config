483e29f8defa2c58ef2c08c737d3b8de4d053d30
JonnerPaz <jonnerpazp@gmail.com>
2023-05-13 14:18:25 -0400

    take back remap.lua from earlier commit w/ git-restore

---
+++ lua/config/remap.lua
@@ -0,0 +1,33 @@
+-- Keybindings file
+-- Leader button
+vim.g.mapleader = " "
+
+-- Explore keymaps
+-- Disable on favor of nvim-tree.
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Fugitive mappings
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+-- harpoon mappings
+-- ../../plugin/harpoon.lua
+
+-- telescope mappings
+-- ../../plugin/telescope.lua
+
+-- tree
+vim.keymap.set("n", "<C-b>", vim.cmd.NvimTreeToggle)
+
+-- trouble mappings
+vim.keymap.set("n", "<leader>tt", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
+--[[ vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true }) ]]
+
+-- undotree
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+-- zen mappings
+vim.keymap.set("n", "<leader>zz", "<cmd>ZenMode<CR>")
