1a949e8f97086e1cbcdfda3c8259da080857cb4e
JonnerPaz <jonnerpazp@gmail.com>
2023-06-11 09:09:53 -0400

    Update kanagawa configs

--- lua/plugins/configs/themes/kanagawa.lua
+++ lua/plugins/configs/themes/kanagawa.lua
@@ -5,21 +5,32 @@ return function()
 		undercurl = true, -- enable undercurls
 		commentStyle = { italic = true },
 		functionStyle = { bold = true },
-		keywordStyle = { bold = true },
+		keywordStyle = {},
 		statementStyle = {},
-		typeStyle = { italic = true },
+		typeStyle = { bold = true },
 		transparent = false, -- do not set background color
 		dimInactive = false, -- dim inactive window `:h hl-NormalNC`
 		terminalColors = true, -- define vim.g.terminal_color_{0,17}
-		colors = { -- add/modify theme and palette colors
+		colors = {
+			-- add/modify theme and palette colors
 			palette = {},
-			theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+			theme = {
+				wave = {},
+				lotus = {},
+				dragon = {},
+				all = {
+					ui = {
+						bg_gutter = "none",
+					},
+				},
+			},
 		},
 		overrides = function(colors) -- add/modify highlights
 			return {}
 		end,
 		theme = "wave", -- Load "wave" theme when 'background' option is not set
-		background = { -- map the value of 'background' option to a theme
+		background = {
+			-- map the value of 'background' option to a theme
 			dark = "wave", -- try "dragon" !
 			light = "lotus",
 		},
