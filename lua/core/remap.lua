60faf904b87fe9d0cc12dbc38326138760502e89
JonnerPaz <jonnerpazp@gmail.com>
2023-06-16 12:15:41 -0400

    Update colors & Add remap configs

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -6,6 +6,12 @@ vim.g.mapleader = " "
 -- Disable on favor of nvim-tree.
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Hard maps
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- Fugitive mappings
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
