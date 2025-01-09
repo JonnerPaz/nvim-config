e30ba38d01f900aa1e8537fb8ba1a6a6c1f7182a
JonnerPaz <jonnerpazp@gmail.com>
2025-01-09 16:19:45 -0400

    Add eslint_d with nvim-lint for javascriptreact/typescriptreact

--- lua/plugins/configs/lsp/lint.lua
+++ lua/plugins/configs/lsp/lint.lua
@@ -1,15 +1,17 @@
 return function()
+	vim.env.ESLINT_D_PPID = vim.fn.getpid()
 	local linter = require("lint")
 
 	linter.linters_by_ft = {
-		bash = { "shellcheck" },
 		typescript = { "eslint_d" },
 		javascript = { "eslint_d" },
+		javascriptreact = { "eslint_d" },
+		typescriptreact = { "eslint_d" },
 	}
 
 	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 		callback = function()
-			return linter.try_lint()
+			linter.try_lint()
 		end,
 	})
 end
