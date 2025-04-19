fd8e958b505274468ab012db09d3080ee7be8b93
JonnerPaz <jonnerpazp@gmail.com>
2025-04-19 12:04:02 -0400

    Add some options to codeium and treesitter

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -2,16 +2,31 @@ return {
 	"Exafunction/codeium.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"hrsh7th/nvim-cmp",
 	},
 	event = "BufEnter",
 	config = function()
 		require("codeium").setup({
-			enabled_cmp_source = false,
+			enabled_cmp_source = true,
+			enable_chat = true,
+			workspace_root = {
+				use_lsp = true,
+				paths = {
+					".git",
+					"package.json",
+				},
+			},
 			virtual_text = {
-				enabled = true,
+				enabled = function()
+					local path = vim.api.nvim_buf_get_name(0)
+					if string.find(path, "oil://", 1, true) == 1 then
+						return false
+					end
+					return true
+				end,
 				key_bindings = {
 					accept = "<Tab>",
+					next = "<M-{>",
+					prev = "<M-}>",
 				},
 			},
 		})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,25 @@
 return {
-    'nvim-treesitter/nvim-treesitter'
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"cpp",
+				"html",
+				"css",
+				"json",
+				"lua",
+				"python",
+				"scss",
+				"tsx",
+				"javascript",
+				"typescript",
+			},
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end,
 }
