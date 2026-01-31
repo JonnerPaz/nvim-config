1f27a45bb051f8064dc19b1f22eba85168107549
JonnerPaz <jonnerpazp@gmail.com>
2026-01-31 13:55:14 -0400

    Get Rid of nvim-dap-ui

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,13 +3,14 @@ return {
 	event = "VeryLazy",
 	dependencies = {
 		"jay-babu/mason-nvim-dap.nvim",
-		{ "rcarriga/nvim-dap-ui", dependencies = { "nvim-neotest/nvim-nio" } },
+		"igorlfs/nvim-dap-view",
+		-- { "rcarriga/nvim-dap-ui", dependencies = { "nvim-neotest/nvim-nio" } },
 		-- Python debugger
 		"mfussenegger/nvim-dap-python",
 	},
 	config = function()
 		local dap = require("dap")
-		local dapui = require("dapui")
+		-- local dapui = require("dapui")
 
 		require("mason-nvim-dap").setup({
 			ensure_installed = { "codelldb", "js-debug-adapter" },
@@ -24,38 +25,86 @@ return {
 		end
 		-- Python debugger --
 
-		dapui.setup()
+		-- dapui.setup()
 
 		dap.listeners.after.event_initialized["dapui_config"] = function()
-			dapui.open()
+			vim.api.nvim_command("DapViewOpen")
 		end
 		dap.listeners.before.event_terminated["dapui_config"] = function()
-			dapui.close()
+			vim.api.nvim_command("DapViewClose")
 		end
 		dap.listeners.before.event_exited["dapui_config"] = function()
-			dapui.close()
+			vim.api.nvim_command("DapViewClose")
 		end
 
 		-- js-debug-adapter
 		local js_dap_path = "~/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js"
-		dap.adapters["pwa-node"] = {
-			type = "server",
-			host = "127.0.0.1",
-			port = "8123",
-			executable = {
-				command = "js-debug-adapter",
-				-- args = { js_dap_path, "${port}" },
-			},
-		}
 
-		for _, language in ipairs({ "typescript", "javascript" }) do
-			require("dap").configurations[language] = {
+		if not dap.adapters["pwa-node"] then
+			dap.adapters["pwa-node"] = {
+				type = "server",
+				host = "127.0.0.1",
+				port = 8123,
+				executable = {
+					command = "js-debug-adapter",
+					-- args = { js_dap_path, "${port}" },
+				},
+			}
+		end
+
+		if not dap.adapters["node"] then
+			dap.adapters["node"] = function(cb, config)
+				if config.type == "node" then
+					config.type = "pwa-node"
+				end
+
+				local nativeAdapter = dap.adapters["pwa-node"]
+				if type(nativeAdapter) == "function" then
+					nativeAdapter(cb, config)
+				else
+					cb(nativeAdapter)
+				end
+			end
+		end
+
+		local js_filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" }
+
+		local vscode = require("dap.ext.vscode")
+		local json = require("plenary.json")
+		local current_file = vim.fn.expand("%:t")
+		vscode.type_to_filetypes["node"] = js_filetypes
+		vscode.type_to_filetypes["pwa-node"] = js_filetypes
+
+		vscode.json_decode = function(str)
+			return vim.json.decode(json.json_strip_comments(str))
+		end
+
+		for _, language in ipairs(js_filetypes) do
+			dap.configurations[language] = {
 				{
 					type = "pwa-node",
 					request = "launch",
 					name = "Launch file",
 					program = "${file}",
-					runtimeExecutable = "node",
+					cwd = "${workspaceFolder}",
+				},
+				{
+					type = "pwa-node",
+					request = "attach",
+					name = "Attach",
+					processId = require("dap.utils").pick_process,
+					cwd = "${workspaceFolder}",
+				},
+				{
+					name = "tsx (" .. current_file .. ")",
+					type = "node",
+					request = "launch",
+					program = "${file}",
+					runtimeExecutable = "tsx",
+					cwd = "${workspaceFolder}",
+					console = "integratedTerminal",
+					internalConsoleOptions = "neverOpen",
+					skipFiles = { "<node_internals>/**", "${workspaceFolder}/node_modules/**" },
 				},
 			}
 		end
