8dac583ca94c6018726b7ea80bb951d7a6a77094
JonnerPaz <jonnerpazp@gmail.com>
2026-01-31 13:52:17 -0400

    Remove gist from fzf search

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -5,9 +5,9 @@ return {
 	config = function()
 		require("fzf-lua").setup({
 			files = {
-				fd_opts = [[--color=never --type f --follow --exclude node_modules --exclude .git]],
-				rg_opts = "--files --hidden --glob '!**/node_modules/*' --glob '!**/.git/*'",
-				file_ignore_pattern = { ".git/", "node_modules/" },
+				fd_opts = [[--color=never --type f --follow --exclude node_modules --exclude .git --exclude dist]],
+				rg_opts = "--files --hidden --glob '!**/node_modules/*' --glob '!**/.git/*' --glob '!**/dist/*'",
+				file_ignore_pattern = { ".git/", "node_modules/", "dist/" },
 			},
 		})
 	end,
