ca223ddeea2442e17e1b9c9cc1d06f31aae854d2
Jonner Paz <jonnerpazp@gmail.com>
2026-03-06 12:08:01 -0400

    feat: Remove markview and minor changes

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -2,15 +2,14 @@
 -- Leader button
 vim.g.mapleader = " "
 
--- Explore keymaps
--- Disable on favor of nvim-tree.
--- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 local opts = { noremap = true, silent = true }
 local remap = vim.keymap.set
 
 -- Hard maps
+-- Center cursor when scrolling in normal mode
 remap("n", "<C-u>", "<C-u>zz")
 remap("n", "<C-d>", "<C-d>zz")
+-- Vscode-like move selection
 remap("v", "J", ":m '>+1<CR>gv=gv")
 remap("v", "K", ":m '<-2<CR>gv=gv")
 remap("n", "n", "nzzzv")
@@ -22,5 +21,6 @@ remap("n", "<C-Down>", ":resize +2<CR>", opts)
 remap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 remap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+-- saving and quitting quickly
 remap("n", "<leader>s", "<cmd>w<cr>", opts)
 remap("n", "<leader>q", "<cmd>qa<cr>", opts)
