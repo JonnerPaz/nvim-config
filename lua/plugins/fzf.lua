c2866b0ca54b6577bbeac370d477d755758ff688
JonnerPaz <jonnerpazp@gmail.com>
2026-04-24 10:47:05 -0400

    fix: Add .cache to fzf ignore for cmake and c++ artifacts

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -5,9 +5,9 @@ return {
 	config = function()
 		require("fzf-lua").setup({
 			files = {
-				fd_opts = [[--color=never --type f --follow --exclude node_modules --exclude .git --exclude dist]],
-				rg_opts = "--files --hidden --glob '!**/node_modules/*' --glob '!**/.git/*' --glob '!**/dist/*'",
-				file_ignore_pattern = { ".git/", "node_modules/", "dist/" },
+				fd_opts = [[--color=never --type f --follow --exclude node_modules --exclude .git --exclude dist --exclude .cache]],
+				rg_opts = "--files --hidden --glob '!**/node_modules/*' --glob '!**/.git/*' --glob '!**/dist/*' --glob '!**/.cache/*'",
+				file_ignore_pattern = { ".git/", "node_modules/", "dist/", ".cache/" },
 			},
 		})
 	end,
