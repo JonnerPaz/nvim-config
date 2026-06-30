1233214440e15085c5c63a724c21a0acf76656bb
JonnerPaz <jonnerpazp@gmail.com>
2026-06-30 12:54:21 -0400

    refactor: Replace copilot.lua with codeium.lua (again)
    I couldn't get copilot to work as I wanted, and I finally found a way to
    use codeium on my new machine. To access codeium, you need to first
    login to windsurf and then run `codeium auth` and it will recognize you
    and sends you the access token, which I didn't have before.

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,43 +1,43 @@
 return {
-	-- "Exafunction/codeium.nvim",
-	-- dependencies = {
-	-- 	"nvim-lua/plenary.nvim",
-	-- },
-	-- event = "BufEnter",
-	-- config = function()
-	-- 	require("codeium").setup({
-	-- 		workspace_root = {
-	-- 			use_lsp = true,
-	-- 			paths = {
-	-- 				".git",
-	-- 				"package.json",
-	-- 			},
-	-- 		},
-	-- 		enable_chat = true,
-	-- 		enable_cmp_source = false,
-	-- 		virtual_text = {
-	-- 			enabled = function()
-	-- 				local path = vim.api.nvim_buf_get_name(0)
-	--
-	-- 				if path.find(path, "oil") == 1 then
-	-- 					return false
-	-- 				end
-	--
-	-- 				return true
-	-- 			end,
-	-- 			key_bindings = {
-	-- 				accept = "<Tab>",
-	-- 				next = "<M-{>",
-	-- 				prev = "<M-}>",
-	-- 			},
-	-- 			filetypes = {
-	-- 				["typescript"] = true,
-	-- 				["oil"] = false,
-	-- 			},
-	-- 			-- virtual_text_priority = 1000,
-	-- 			map_keys = true,
-	-- 			default_filetype_enabled = true,
-	-- 		},
-	-- 	})
-	-- end,
+	"Exafunction/codeium.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	event = "BufEnter",
+	config = function()
+		require("codeium").setup({
+			workspace_root = {
+				use_lsp = true,
+				paths = {
+					".git",
+					"package.json",
+				},
+			},
+			enable_chat = true,
+			enable_cmp_source = false,
+			virtual_text = {
+				enabled = function()
+					local path = vim.api.nvim_buf_get_name(0)
+
+					if path.find(path, "oil") == 1 then
+						return false
+					end
+
+					return true
+				end,
+				key_bindings = {
+					accept = "<Tab>",
+					next = "<M-{>",
+					prev = "<M-}>",
+				},
+				filetypes = {
+					["typescript"] = true,
+					["oil"] = false,
+				},
+				-- virtual_text_priority = 1000,
+				map_keys = true,
+				default_filetype_enabled = true,
+			},
+		})
+	end,
 }
